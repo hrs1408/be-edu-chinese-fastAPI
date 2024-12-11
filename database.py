c124ba035ddba2c109438db727fc74b461d6f7b2
TRUONG DINH QUYEN <kymonos1@gmail.com>
2024-12-11 16:31:44 +0700

    path database

--- database.py
+++ database.py
@@ -1,9 +1,10 @@
-from sqlmodel import create_engine, SQLModel, Session
+from sqlmodel import create_engine, SQLModel
 
 
-DATABASE_URL = "sqlite:///db.sqlite"
+# DATABASE_URL = "sqlite:///db.sqlite"
+DATABASE_URL = "mysql+pymysql://an1rs09vlfub_admin_dengfuchinese:dengfuchinese%40123@137.59.105.45:3306/an1rs09vlfub_dengfuchinese"
 
-engine = create_engine(DATABASE_URL, echo=True, connect_args={"check_same_thread": False})
+engine = create_engine(DATABASE_URL, echo=True)
 
-def init_db():
+def init_db():  
     SQLModel.metadata.create_all(engine)
